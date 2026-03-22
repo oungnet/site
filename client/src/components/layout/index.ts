e23a22ca0e597d58f3c54f6c9af088f489dc8cdc
Oung <77523417+oungnet@users.noreply.github.com>
2026-03-23 04:18:21 +0700

    Create layout index file

---
+++ client/src/components/layout/index.ts
@@ -0,0 +1,5 @@
+// Export all layout components here for easy importing
+export { default as Navigation } from '../Navigation';
+export { default as Footer } from '../Footer'];
+
+// Add more layout components as needed
