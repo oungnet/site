2c012ecc84e56e8fa69d8cf4e07d7ccd1f62176a
Oung <77523417+oungnet@users.noreply.github.com>
2026-03-23 05:06:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { join } from 'path';
 
 export default defineConfig({
+  base: "/site/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': join(__dirname, 'client/src'),
+      '@': join(__dirname, 'src'), // ปรับให้ตรงกับโครงสร้างจริง
     },
   },
   server: {
